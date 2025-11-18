822e9812f04ae46ec6eb192c84e6e72dd27d404c
Brendan Wood <woodbrendan@hotmail.com>
2025-11-18 16:27:04 +0000

    Fix back button not working

--- lib/controllers/carplay_controller.dart
+++ lib/controllers/carplay_controller.dart
@@ -153,8 +153,10 @@ class FlutterCarPlayController {
     l1:
     for (var t in templateHistory) {
       if (t is CPListTemplate) {
-        barButton = t.backButton;
-        break l1;
+        if (t.backButton?.uniqueId == elementId) {
+          barButton = t.backButton;
+          break l1;
+        }
       }
     }
     if (barButton != null) barButton.onPress();
